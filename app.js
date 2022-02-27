edf01389b76602f6203574508ae45f8e29387c83
Hashim Sharif <sharif.hashim@gmail.com>
2022-02-26 19:37:07 -0500

    change path from public to routes

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "routes")));
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
